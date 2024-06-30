23fb7c98af13dc83b3d83d0c630482528dd9c367
Jaleel Bennett <jaleelbennett58@gmail.com>
2024-06-30 18:02:47 -0400

    refactor(article-viewer): sanitizing html before passing to article viewer

--- components/article-viewer.tsx
+++ components/article-viewer.tsx
@@ -23,40 +23,8 @@ import {
 } from "@/lib/tiptap-extensions";
 
 export function ArticleViewer({ content }: { content: string }) {
-  const safeHTMLContent = DOMPurify.sanitize(content || "", {
-    USE_PROFILES: { html: true },
-    ALLOWED_ATTR: [
-      "class",
-      "style",
-      "src",
-      "alt",
-      "title",
-      "href",
-      "target",
-      "rel",
-      "data-src",
-      "data-href",
-      "data-title",
-      "data-alt",
-      "data-target",
-      "status",
-      "data-status",
-      "previewListener",
-      "data-previewListener",
-      "data-embed",
-      "data-embed-type",
-      "data-embed-id",
-      "data-embed-url",
-      "data-embed-provider",
-      "data-embed-thumbnail",
-      "data-embed-title",
-      "data-bg",
-      "data-ll-status",
-    ],
-  });
-
   const editor = useEditor({
-    content: safeHTMLContent,
+    content: content,
     editable: false, // Make it non-editable initially
     extensions: [
       StarterKit.configure({
--- components/article.tsx
+++ components/article.tsx
@@ -9,8 +9,41 @@ import { calculateReadTime, formatDate } from "@/lib/utils";
 import { Button } from "./ui/button";
 import { Icons } from "./icons";
 import { ArticleViewer } from "./article-viewer";
+import DOMPurify from "dompurify";
 
 export function Article({ content }: { content: ArticleDetails }) {
+  const safeHTMLContent = DOMPurify.sanitize(content?.content || "", {
+    USE_PROFILES: { html: true },
+    ALLOWED_ATTR: [
+      "class",
+      "style",
+      "src",
+      "alt",
+      "title",
+      "href",
+      "target",
+      "rel",
+      "data-src",
+      "data-href",
+      "data-title",
+      "data-alt",
+      "data-target",
+      "status",
+      "data-status",
+      "previewListener",
+      "data-previewListener",
+      "data-embed",
+      "data-embed-type",
+      "data-embed-id",
+      "data-embed-url",
+      "data-embed-provider",
+      "data-embed-thumbnail",
+      "data-embed-title",
+      "data-bg",
+      "data-ll-status",
+    ],
+  });
+
   return (
     <article className="w-full">
       <section className="container px-0 md:px-8 flex flex-col items-center gap-12">
@@ -70,7 +103,7 @@ export function Article({ content }: { content: ArticleDetails }) {
           <h1 className="font-heading text-2xl md:text-3xl lg:text-4xl">
             {content?.title}
           </h1>
-          {content?.content && <ArticleViewer content={content.content} />}
+          {safeHTMLContent && <ArticleViewer content={safeHTMLContent} />}
         </article>
       </section>
     </article>
