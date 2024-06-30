8ec4bac5e52277d6ae6f0e5feec4e1bb006b3f7c
Jaleel Bennett <jaleelbennett58@gmail.com>
2024-06-30 15:47:04 -0400

    feat: font assets and config

---
+++ lib/fonts.ts
@@ -0,0 +1,17 @@
+import { Inter, JetBrains_Mono } from "next/font/google";
+import localFont from "next/font/local";
+
+export const fontMono = JetBrains_Mono({
+  subsets: ["latin"],
+  variable: "--font-mono",
+});
+
+export const fontHeading = localFont({
+  src: "../assets/fonts/CalSans-SemiBold.woff2",
+  variable: "--font-heading",
+});
+
+export const fontSans = Inter({
+  subsets: ["latin"],
+  variable: "--font-sans",
+});
