5163052b55021a5c319c377de11952f2c67b8a2c
Jaleel Bennett <jaleelbennett58@gmail.com>
2024-06-30 17:27:47 -0400

    chore(config): medium image domain config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "miro.medium.com",
+        port: "",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
