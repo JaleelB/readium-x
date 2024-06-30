92d3809dd8e77dcccb2b11a6b53fc6b87560742d
Jaleel Bennett <jaleelbennett58@gmail.com>
2024-06-30 15:55:17 -0400

    feat(website): site config

---
+++ config/site.ts
@@ -0,0 +1,22 @@
+export type SiteConfig = typeof siteConfig;
+
+export const siteConfig = {
+  name: "ReadiumX - Read and Manage Premium Medium Articles for Free",
+  short_name: "ReadiumX",
+  description:
+    "An open source tool that provides access to premium Medium articles without the paywall, allowing you to bookmark, and manage your reading experience across any device.",
+  url: "https://readiumx.vercel.app",
+  ogImage: "https://readiumx.vercel.app/og.png",
+  links: {
+    twitter: "https://twitter.com/jal_eelll",
+    github: "https://github.com/JaleelB/readium-x",
+  },
+  creator: {
+    name: "Jaleel Bennett",
+    twitter: "https://twitter.com/jal_eelll",
+    twitterId: "@jal_eelll",
+    github: "https://github.com/JaleelB",
+    website: "https://jaleelbennett.com",
+    mail: "jaleelcodes@gmail.com",
+  },
+};
