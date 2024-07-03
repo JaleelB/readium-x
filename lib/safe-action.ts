8b3c475fa3a81a0c117c5430cfa49af51b9eecda
Jaleel Bennett <jaleelbennett58@gmail.com>
2024-07-02 21:56:10 -0400

    feat(lib): enhance utility and action handling

---
+++ lib/safe-action.ts
@@ -0,0 +1,35 @@
+import { env } from "@/env";
+import { assertAuthenticated } from "@/lib/session";
+import { createServerActionProcedure } from "zsa";
+import { PublicError } from "../use-cases/errors";
+
+function shapeErrors({ err }: any) {
+  const isAllowedError = err instanceof PublicError;
+  // let's all errors pass through to the UI so debugging locally is easier
+  const isDev = env.NODE_ENV === "development";
+  if (isAllowedError || isDev) {
+    console.error(err);
+    return {
+      code: err.code ?? "ERROR",
+      message: `${isDev ? "DEV ONLY ENABLED - " : ""}${err.message}`,
+    };
+  } else {
+    return {
+      code: "ERROR",
+      message: "Something went wrong",
+    };
+  }
+}
+
+export const authenticatedAction = createServerActionProcedure()
+  .experimental_shapeError(shapeErrors)
+  .handler(async () => {
+    const user = await assertAuthenticated();
+    return { user };
+  });
+
+export const unauthenticatedAction = createServerActionProcedure()
+  .experimental_shapeError(shapeErrors)
+  .handler(async () => {
+    return { user: undefined };
+  });
---
+++ lib/session.ts
@@ -0,0 +1,33 @@
+import "server-only";
+import { cookies } from "next/headers";
+import { lucia } from "@/server/auth";
+import { validateRequest } from "@/server/auth";
+import { cache } from "react";
+import { AuthenticationError } from "../use-cases/errors";
+import { UserId } from "@/use-cases/types";
+
+export const getCurrentUser = cache(async () => {
+  const session = await validateRequest();
+  if (!session.user) {
+    return undefined;
+  }
+  return session.user;
+});
+
+export const assertAuthenticated = async () => {
+  const user = await getCurrentUser();
+  if (!user) {
+    throw new AuthenticationError();
+  }
+  return user;
+};
+
+export async function setSession(userId: UserId) {
+  const session = await lucia.createSession(userId, {});
+  const sessionCookie = lucia.createSessionCookie(session.id);
+  cookies().set(
+    sessionCookie.name,
+    sessionCookie.value,
+    sessionCookie.attributes
+  );
+}
