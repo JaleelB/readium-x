1d057b1579ddabd546cad042133ed113a159c166
Jaleel Bennett <jaleelbennett58@gmail.com>
2024-07-08 23:24:50 -0400

    fix(schema): made article schema values nullable

--- schemas/article.ts
+++ schemas/article.ts
@@ -3,11 +3,11 @@ import { z } from "zod";
 export const articleSchema = z.object({
   title: z.string(),
   content: z.string(),
-  articleImageSrc: z.string().url(),
-  authorName: z.string(),
-  authorImageURL: z.string().url(),
-  authorProfileURL: z.string().url(),
-  publicationName: z.string(),
-  readTime: z.string(),
-  publishDate: z.string(),
+  articleImageSrc: z.string().url().nullable(),
+  authorName: z.string().nullable(),
+  authorImageURL: z.string().url().nullable(),
+  authorProfileURL: z.string().url().nullable(),
+  publicationName: z.string().nullable(),
+  readTime: z.string().nullable(),
+  publishDate: z.string().nullable(),
 });
