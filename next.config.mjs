11980ae78caeb29281cea974e28f7261b915beef
Jaleel Bennett <jaleelbennett58@gmail.com>
2024-07-11 12:46:16 -0400

    feat(config): validating env schema at build time

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,9 @@
+import createJiti from "jiti";
+const jiti = createJiti(fileURLToPath(import.meta.url));
+
+// Import env here to validate during build. Using jiti we can import .ts files :)
+jiti("./env");
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "standalone",
