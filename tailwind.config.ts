3021b719138e0bea89b325b61deb92b8671fa900
Jaleel Bennett <jaleelbennett58@gmail.com>
2024-07-18 10:20:36 -0400

    fix(config): including tiptap extension file in allowed tailwind files content

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,6 +7,7 @@ const config = {
     "./pages/**/*.{ts,tsx}",
     "./components/**/*.{ts,tsx}",
     "./app/**/*.{ts,tsx}",
+    "./lib/**/*.{ts,tsx}",
     "./src/**/*.{ts,tsx}",
   ],
   prefix: "",
