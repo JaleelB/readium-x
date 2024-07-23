2e3f170c60b24f91f32108851da74b3018482380
Jaleel Bennett <jaleelbennett58@gmail.com>
2024-07-23 07:57:37 -0400

    feat(seo): robots txt file

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,8 +1,13 @@
+import { env } from "@/env";
 import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
+}
+
+export function absoluteUrl(path: string) {
+  return `${env.NEXT_PUBLIC_APP_URL}${path}`;
 }
 
 export function calculateReadTime(text: string[] | string | undefined): string {
