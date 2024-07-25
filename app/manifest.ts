04baf004171ecb89811632561b38d67d0d6bd734
Jaleel Bennett <jaleelbennett58@gmail.com>
2024-07-24 23:13:48 -0400

    refactor(manifest): use next js built-in manifest generation

---
+++ app/manifest.ts
@@ -0,0 +1,31 @@
+import { MetadataRoute } from "next";
+import { siteConfig } from "@/app-config";
+
+export default function manifest(): MetadataRoute.Manifest {
+  return {
+    name: siteConfig.name,
+    short_name: siteConfig.short_name,
+    description: siteConfig.description,
+    start_url: "/",
+    icons: [
+      {
+        src: "/favicon.ico",
+        sizes: "any",
+        type: "image/x-icon",
+      },
+      {
+        src: "/android-chrome-192x192.png",
+        sizes: "192x192",
+        type: "image/png",
+      },
+      {
+        src: "/android-chrome-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+      },
+    ],
+    theme_color: "#ffffff",
+    background_color: "#ffffff",
+    display: "standalone",
+  };
+}
