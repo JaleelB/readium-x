659365e86e9b6aeeb144b205dd5b4c9f477111c2
Jaleel Bennett <jaleelbennett58@gmail.com>
2024-08-06 23:02:22 -0400

    feat(layout): settings layout

---
+++ app/account/settings/setting-layout.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import { ReactNode } from "react";
+import { cn } from "@/lib/utils";
+import NavLink from "@/components/settings-nav-link";
+
+export interface Tab {
+  name: string;
+  segment: string | null;
+}
+
+interface SettingsLayoutProps {
+  tabs: Tab[];
+  children: ReactNode;
+}
+
+export default function SettingsLayout({
+  tabs,
+  children,
+}: SettingsLayoutProps) {
+  return (
+    <div className="mx-auto flex h-full w-full max-w-screen-2xl flex-1 flex-col gap-6 lg:px-20">
+      <nav className={cn("py-4")}>
+        <ul className="scrollbar-hide flex space-x-6 overflow-auto text-sm">
+          {tabs.map((tab) => (
+            <li key={tab.segment}>
+              <NavLink key={tab.segment} segment={tab.segment}>
+                {tab.name}
+              </NavLink>
+            </li>
+          ))}
+        </ul>
+      </nav>
+      <div className="grid gap-5 lg:col-span-4">{children}</div>
+    </div>
+  );
+}
