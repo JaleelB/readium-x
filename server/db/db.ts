b2a3c5e40e82671727cfd3f7d433d07473e6650b
Jaleel Bennett <jaleelbennett58@gmail.com>
2026-05-04 23:22:27 -0400

    local sqlite database

--- server/db/db.ts
+++ server/db/db.ts
@@ -4,9 +4,16 @@ import { drizzle } from "drizzle-orm/libsql";
 import { createClient } from "@libsql/client";
 import { env } from "@/env";
 
+const url =
+  process.env.NODE_ENV === "development"
+    ? "file:./server/db/local.db"
+    : env.DATABASE_URL;
+const authToken =
+  process.env.NODE_ENV === "development" ? undefined : env.DB_AUTH_TOKEN;
+
 export const client = createClient({
-  url: env.DATABASE_URL!,
-  authToken: env.DB_AUTH_TOKEN!,
+  url: url!,
+  authToken: authToken,
 });
 
 export const db = drizzle(client, { schema });
